462c0cf97348cba31cee579793952686e6811cb2
Dennis Kipruto Sambu <dennoh86@gmail.com>
2026-08-31 23:58:53 +0300

    Fetch and render toys on page load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
+const url = "http://localhost:3001/toys";
+
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
+
+  function updateToy(updatedToy){
+    setToys((toys) => toys.map((toy) => toy.id === updatedToy.id ? updatedToy : toy ));
+  }
+
+  function removeToy (id) {
+    setToys((toys) => toys.filter((toy)=> toy.id !==id))
+  }
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setToys(data))
+      .catch((err) => console.error("Failed to fetch", err));
+  }, []);
 
   return (
     <>
@@ -18,7 +36,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} updateToy={updateToy} removeToy={removeToy}/>
     </>
   );
 }
