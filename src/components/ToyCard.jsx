8aa74118c2cc3bfbfc6b0bb4108d2bb25c131e98
Dennis Kipruto Sambu <dennoh86@gmail.com>
2026-09-03 01:00:30 +0300

    Add like functionality via PATCH request

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -10,12 +10,24 @@ function ToyCard({ toy, removeToy, updateToy }) {
     }).then(() => removeToy(id));
   }
 
+  function handleClick() {
+    fetch(`${url}/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ likes: likes + 1 }),
+    })
+      .then((res) => res.json())
+      .then((data) => updateToy(data));
+  }
+
   return (
     <div className="card" data-testid="toy-card">
       <h2>{name}</h2>
       <img src={image} alt={name} className="toy-avatar" />
       <p>{likes} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
+      <button className="like-btn" onClick={handleClick}>
+        Like {"<3"}
+      </button>
       <button className="del-btn" onClick={handleDeleteClick}>
         Donate to GoodWill
       </button>
